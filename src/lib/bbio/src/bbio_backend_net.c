d70facf607217c2c47e621987ab76648d3ee4c03
ChengYe <666@orgleaf.com>
2024-09-12 13:01:47 +0800

    Open stream callback

--- src/lib/bbio/src/bbio_backend_net.c
+++ src/lib/bbio/src/bbio_backend_net.c
@@ -15,6 +15,16 @@
 
 #define SOCK_FLAGS 0 //(MSG_ZEROCOPY)
 
+handshake_msg mapped_handshake_msg = {
+    .head = HANDSHAKE_HEADER,
+    .type = 0,
+};
+
+handshake_msg stream_handshake_msg = {
+    .head = HANDSHAKE_HEADER,
+    .type = 1,
+};
+
 typedef struct _open_call_param
 {
     uint8_t file_path_len;
@@ -42,16 +52,16 @@ int io_net_call_create(io_net_context *ctx, enum cmd_call_type type)
 #define _call_print(...)
 #endif
 
-#define io_net_call_start(ctx, type)                                    \
-    {                                                                   \
-        uint8_t type_value = ((uint8_t)type << 1) | (0 & 0b1);          \
-        _call_print("Call %s %d\n", #type, type);                       \
+#define io_net_call_start(ctx, type)                                \
+    {                                                               \
+        uint8_t type_value = ((uint8_t)type << 1) | (0 & 0b1);      \
+        _call_print("Call %s %d\n", #type, type);                   \
         send(ctx->fd, &type_value, sizeof(type_value), SOCK_FLAGS); \
     }
 #define io_net_call_param(ctx, size, value)                  \
     {                                                        \
         _call_print("    \tparam len=%d %p\n", size, value); \
-        send(ctx->fd, value, size, SOCK_FLAGS);          \
+        send(ctx->fd, value, size, SOCK_FLAGS);              \
     }
 
 #define io_net_call_startv(ctx, iov, type)                     \
@@ -67,23 +77,16 @@ int io_net_call_create(io_net_context *ctx, enum cmd_call_type type)
         iov.iov_base = value;                                \
         iov.iov_len = size;                                  \
     }
-// int io_net_call_param(io_net_context *ctx, size_t size, void *value)
-// {
-//     int n;
-//     printf("    \tparam len=%d\n", size);
-//     n = send(ctx->sockfd, value, size, 0);
-//     return n;
-// }
+
 #define io_net_call_return_value(ctx, size, ret) \
     {                                            \
-        recv(ctx->fd, ret, size, 0);         \
+        recv(ctx->fd, ret, size, 0);             \
     }
 #define io_net_call_return_null(ctx) \
     {                                \
     }
 #define GET_MACRO(_1, _2, _3, NAME, ...) NAME
 #define io_net_call_return(...) GET_MACRO(__VA_ARGS__, io_net_call_return_value, _, io_net_call_return_null)(__VA_ARGS__)
-
 
 void io_write_net(io_mapped_device *device, uint32_t addr, uint32_t value)
 {
@@ -104,24 +107,63 @@ uint32_t io_read_net(io_mapped_device *device, uint32_t addr)
     return value;
 }
 
+static struct channel_buffer *io_stream_alloc_buffer_net(io_stream_device *device, uint32_t flag)
+{
+    // printf("get buffer\n");
+    return (struct channel_buffer *)device->ch.private;
+}
 
 static uint32_t io_write_stream_net(io_stream_device *device, void *data, uint32_t size)
 {
     int flag_on = 1, flag_off = 0;
-    struct iovec iov[4];
-    io_net_call_startv(device, iov[0], CALL_WRITE_BURST);
-    io_net_call_paramv(device, iov[2], sizeof(size), &size);
-    io_net_call_paramv(device, iov[3], size, data);
-    setsockopt(device->fd, IPPROTO_TCP, TCP_CORK, &flag_on, sizeof(flag_on));
-    writev(device->fd, iov, 4);
-    setsockopt(device->fd, IPPROTO_TCP, TCP_CORK, &flag_off, sizeof(flag_off));
-    io_net_call_return(device, sizeof(size), &size);
+    // struct iovec iov[3];
+    // io_net_call_startv(device, iov[0], CALL_WRITE_STREAM);
+    // io_net_call_paramv(device, iov[1], sizeof(size), &size);
+    // io_net_call_paramv(device, iov[2], size, data);
+
+    // writev(device->fd, iov, 3);
+    io_net_call_param(device, size, data);
+    // io_net_call_return(device, sizeof(size), &size);
     return size;
 }
 
+static int io_handshake(int sockfd, enum cmd_channel_type type)
+{
+    handshake_msg msg = {
+        .head = HANDSHAKE_HEADER,
+        .type = type,
+    };
+    uint32_t ack = 0;
+    int n;
+
+    send(sockfd, &msg, sizeof(msg), SOCK_FLAGS);
+    n = recv(sockfd, &ack, sizeof(ack), SOCK_FLAGS);
+    if ((n <= 0) || (ack != HANDSHAKE_HEADER))
+        return -1;
+    printf("Handshake ok\n");
+    return 0;
+}
+
+static IO_FD io_open_remote_device(int sockfd, char *dev_path, size_t size)
+{
+    uint32_t path_len;
+    IO_FD fd;
+    int n;
+    // open device
+    path_len = strlen(dev_path);
+    printf("open remote %d %s %d\n", path_len, dev_path, size);
+    send(sockfd, &path_len, sizeof(path_len), 0);
+    send(sockfd, dev_path, path_len, 0);
+    send(sockfd, &size, sizeof(size), 0);
+    n = recv(sockfd, &fd, sizeof(fd), 0);
+    if (n <= 0 || (fd <= 0))
+        return -1;
+
+    return fd;
+}
 io_mapped_device *io_open_mapped_net(io_context *ctx, char *file_path, size_t size)
 {
-    int sockfd;
+    int sockfd, remotefd;
     int ret;
     const int flag = 1;
     io_net_context *ctx_net = (io_net_context *)ctx;
@@ -142,6 +184,11 @@ io_mapped_device *io_open_mapped_net(io_context *ctx, char *file_path, size_t si
     setsockopt(sockfd, SOL_SOCKET, SO_ZEROCOPY, &flag, sizeof(flag));
 
     // handshake
+    printf("Handshake...\n");
+    if (io_handshake(sockfd, MAPPED_CHANNEL))
+        return NULL;
+    // send file info
+    remotefd = io_open_remote_device(sockfd, file_path, size);
 
     device = (io_mapped_device *)malloc(sizeof(io_mapped_device));
     device->fd = sockfd;
@@ -152,11 +199,9 @@ io_mapped_device *io_open_mapped_net(io_context *ctx, char *file_path, size_t si
     return device;
 }
 
-
-
-io_stream_device *io_open_stream_net(io_context *ctx, char *file_path, size_t size)
+io_stream_device *io_open_stream_net(io_context *ctx, char *file_path)
 {
-    int sockfd;
+    int sockfd, remotefd;
     int ret;
     const int flag = 1;
     io_net_context *ctx_net = (io_net_context *)ctx;
@@ -166,6 +211,7 @@ io_stream_device *io_open_stream_net(io_context *ctx, char *file_path, size_t si
     if (sockfd < 0)
         return NULL;
 
+    printf("Connect stream for device %s\n", file_path);
     ret = connect(sockfd, (struct sockaddr *)&ctx_net->serv_addr, sizeof(ctx_net->serv_addr));
 
     if (ret < 0)
@@ -177,12 +223,25 @@ io_stream_device *io_open_stream_net(io_context *ctx, char *file_path, size_t si
     setsockopt(sockfd, SOL_SOCKET, SO_ZEROCOPY, &flag, sizeof(flag));
 
     // handshake
+    if (io_handshake(sockfd, STREAM_CHANNEL))
+        return NULL;
+    // send file info
+    printf("Open remote...");
+    remotefd = io_open_remote_device(sockfd, file_path, 0);
 
+    if (remotefd < 1)
+        return NULL;
+    printf("Remote device %s open ok\n", file_path);
 
-    device = (io_stream_device *) malloc(sizeof(io_stream_device));
+    device = (io_stream_device *)malloc(sizeof(io_stream_device));
     device->fd = sockfd;
 
     device->ch.write_stream = io_write_stream_net;
+    device->ch.private = (void *)malloc(sizeof(struct channel_buffer));
+    device->ch.alloc_buffer = io_stream_alloc_buffer_net;
+
+    setsockopt(device->fd, IPPROTO_TCP, TCP_CORK, &flag, sizeof(flag));
+    io_net_call_start(device, CALL_WRITE_STREAM);
 
     return device;
 }
--- src/lib/bbio/src/bbio_net_core.c
+++ src/lib/bbio/src/bbio_net_core.c
@@ -6,10 +6,11 @@
 
 io_context *io_create_net_context(char *host, uint16_t port)
 {
-    struct sockaddr_in serv_addr;
     struct hostent *server;
     int ret;
     char addr_str[INET_ADDRSTRLEN];
+
+    io_net_context *ctx_net = (io_net_context *)malloc(sizeof(io_net_context));
 
     server = gethostbyname(host);
     if (server == NULL)
@@ -21,18 +22,19 @@ io_context *io_create_net_context(char *host, uint16_t port)
     inet_ntop(AF_INET, server->h_addr_list[0], addr_str, INET_ADDRSTRLEN);
     printf("Server %s resolved to %s\n", host, addr_str);
 
-    memset((char *)&serv_addr, 0, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
 
-    memcpy((char *)&serv_addr.sin_addr.s_addr, (char *)server->h_addr_list[0], server->h_length);
-    serv_addr.sin_port = htons(port);
+    memset((char *)&ctx_net->serv_addr, 0, sizeof(ctx_net->serv_addr));
+    ctx_net->serv_addr.sin_family = AF_INET;
 
+    memcpy((char *)&ctx_net->serv_addr.sin_addr.s_addr, (char *)server->h_addr_list[0], server->h_length);
+    ctx_net->serv_addr.sin_port = htons(port);
 
-    io_net_context *ctx_net = (io_net_context *)malloc(sizeof(io_net_context));
+    
     io_context *ctx = (io_context *)ctx_net;
 
     ctx->devices = (io_device **)malloc(1 * sizeof(io_device *));
     ctx->backend.open_mapped = io_open_mapped_net;
+    ctx->backend.open_stream = io_open_stream_net;
     // ctx_net->sockfd = sockfd;
     // ctx->backend.open = io_open_net;
     // ctx->backend.mmap = io_mmap_net;
@@ -48,3 +50,5 @@ io_context *io_create_net_context(char *host, uint16_t port)
     }
     return ctx;
 }
+
+
